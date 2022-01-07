16ad3423e8888a6bf2e0765bdb4139ec46dd6463
ooooo5h <ooooo5h91@gmail.com>
2022-01-07 16:52:49 +0900

    API 호출 결과 확인

--- main.py
+++ main.py
@@ -27,4 +27,8 @@ def send_fcm_notification(title, body) :
     }
     
     # 3. 어떤 방식 + 실제 API 호출
-    requests.post(fcm_url, data=json.dumps(content), headers=fcm_headers)
+    result = requests.post(fcm_url, data=json.dumps(content), headers=fcm_headers)
+    print(f'FCM 발송 결과 : {result}')
+    
+
+send_fcm_notification('안녕하세요', '파이썬을 통해서 보내지롱')
